1513dc0122a133499ca8f342eb64dc4b0088d0ae
Barbara.Roda <b.roda.br94@gmail.com>
2024-01-05 20:45:24 -0300

    Corregido

--- 06-07-08.js
+++ 06-07-08.js
@@ -11,6 +11,11 @@ function crearClasePersona() {
       // Inicializar las propiedades de la persona con los valores recibidos como argumento
 
       // Tu código aca:
+      this.nombre = nombre;
+      this.edad = edad;
+      this.hobbies = hobbies;
+      this.amigos = amigos;
+
 
     }
 
@@ -20,6 +25,7 @@ function crearClasePersona() {
       // No debe retornar nada.
 
       // Tu código aca:
+      this.amigos.push({ nombre: nombre, edad: edad });
 
     }
 
@@ -28,6 +34,7 @@ function crearClasePersona() {
       // No debe retornar nada.
 
       // Tu código aca:
+      this.hobbies.push(hobbie);
 
     }
     getFriends() {
@@ -38,6 +45,7 @@ function crearClasePersona() {
       // persona.getFriends() debería devolver ['martin', 'toni']
 
       // Tu código aca:
+      return this.amigos.map(amigo => amigo.nombre)
 
     }
 
@@ -47,6 +55,7 @@ function crearClasePersona() {
       // persona.getHobbies() debe devolver ['correr', 'dormir', 'nadar']
 
       // Tu código aca:
+      return this.hobbies;
 
     }
 
@@ -66,6 +75,11 @@ function crearClasePersona() {
       // persona.getPromedioEdad() debería devolver 29 ya que (33 + 25) / 2 = 29
 
       // Tu código aca:
+      if (this.amigos.length === 0) {
+        return 0;
+      }
+      Const sumaEdades = this.amigos.reduce((total, amigo) => total + amigo.edad, 0);
+      return sumaEdades / this.amigos.length;
 
     }
   };
